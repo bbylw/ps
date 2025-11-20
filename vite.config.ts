fc31463535e36c2fff03b1fb4250b62b537d0ae8
free <30594873+bbylw@users.noreply.github.com>
2025-11-20 20:43:37 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bbylw/ps/', // 注意：前后都需要斜杠，例如 '/webnav-hub/'
+  // 这里的 base 必须和你的 GitHub 仓库名称一致，且前后都要有斜杠
+  // 如果你的仓库名是 'ps'，这里就填 '/ps/'
+  base: '/ps/', 
   plugins: [react()],
 })
