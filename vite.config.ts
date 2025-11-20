84ec349e56352b0d4386dc619dd0629c2cf137c9
free <30594873+bbylw@users.noreply.github.com>
2025-11-20 21:12:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 这里的 base 必须和你的 GitHub 仓库名称一致，且前后都要有斜杠
-  // 如果你的仓库名是 'ps'，这里就填 '/ps/'
-  base: '/ps/', 
+  // 修改为 './' (相对路径)
+  // 这样构建出来的资源引用会是 src="./assets/..."
+  // 它可以自动适配 GitHub Pages (子路径 /ps/) 和 Cloudflare Pages (根路径 /)
+  base: './', 
   plugins: [react()],
 })
